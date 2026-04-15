0489ddb7732d580ea00478fb21af8738afba6ccd
Chandan D Karmaker <chandankarmaker555@gmail.com>
2026-04-15 16:53:10 +0600

    added pie chart to visualise overall iteractions

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -1,18 +1,33 @@
 'use client'
-import React, { useContext } from 'react';
-import { friendContext } from '../contextAPI/friendContext';
+
+import PieChart from "@/components/PieChart";
 
 const StatsPage = () => {
 
-    const { interaction } = useContext(friendContext);
-    console.log("Interaction:", interaction);
 
     return (
-        <div>
+        <div className="space-y-6">
             <h3 className='text-5xl font-bold text-[#1F2937]'>Friendship Analytics</h3>
 
-            <div>
-
+            <div className="p-10 bg-base-100">
+                <h1 className="text-xl font-medium text-[#244D3F]">By Interaction Type</h1>
+                <div className="py-4 flex items-center justify-center">
+                    <PieChart />
+                </div>
+                <div className="flex justify-center items-center gap-4">
+                    <div className="flex gap-2 items-center">
+                        <div className="p-2 bg-[#244D3F] rounded-full"></div>
+                        <p>Call</p>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <div className="p-2 bg-[#7E35E1] rounded-full"></div>
+                        <p>SMS</p>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <div className="p-2 bg-[#37A163] rounded-full"></div>
+                        <p>Video Call</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
